287be706b7468832bc087f6e47c11699f28560a7
EC2 Default User <ec2-user@ip-172-16-72-98.ap-southeast-1.compute.internal>
2019-11-27 16:29:29 +0000

    completed project

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,9 +69,9 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    a, b = convert_and_pad(model.word_dict, review_to_words(input_data))
+    data_X = a
+    data_len = b
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +87,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    result = model(data).round().item()
 
     return result
